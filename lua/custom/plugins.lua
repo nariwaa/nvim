17d24443449b109258ad755218b0c5b9f23fee42
nariwaa <lucasval3n2c@gmail.com>
2024-11-25 14:58:44 +0100

    added showkeys

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -3,5 +3,14 @@ local plugins = {
     "xiyaowong/transparent.nvim",
     lazy = false
   },
+  {
+    "nvzone/showkeys",
+    cmd = "ShowkeysToggle",
+    opts = {
+      timeout = 1,
+      maxkeys = 5,
+      position = "bottom-left"
+    }
+  },
 }
 return plugins
