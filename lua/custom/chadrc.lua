b82dac99e23757d31918a4c072ca66f05232332a
nariwaa <lucasval3n2c@gmail.com>
2024-11-25 17:08:38 +0100

    added nvdash

--- lua/custom/chadrc.lua
+++ lua/custom/chadrc.lua
@@ -1,7 +1,27 @@
 ---@type ChadrcConfig
+
 local M = {}
 
-M.ui = {
+local ascii_art_1 = {
+  "       ⊹⠀⠀⠀ ⣤⣶⠿⢖⣢⣴⣶⣦⣤⣄         ",
+  "       ⠀⠀⠀⢸⡟⢁⣼⠟⣩⢤⣤⠤⣉⠻⣇⠀     ˖  ",
+  "       ⢀⣾⣿⡈⡇⢸⡏⣼⣇⡑⢀⠇⣸⢆⢿⢸⣿⣦      ",
+  "       ⣸⣿⣿⡇⠁⣀⡘⠿⠿⠿⢣⡾⢏⣼⠈⣼⣿⣿      ",
+  "       ⣿⣿⣿⣄⠾⣿⣿⣿⣶⣶⣤⣶⣿⡟⠀⣿⣿⠏      ",
+  "       ⠈⠙⠿⣿⣦⡈⢿⣿⣿⣿⣿⡿⠋⣠⣾⣿⣷       ",
+  "       ⠀⠀⠀⠈⠉⠉⢀⣉⣉⣩⣥⣶⣿⣿⡿⠟⠋       ",
+  "  ⁺⊹     ⠀⠀⠀⠀⠀ ⠻⣿⣿⣿⠟⠉⠀⠀    ⊹   ",
+  "                               ",
+  "      Powered By eovim      ",
 }
+
+
+M.ui = {
+  nvdash = {
+    load_on_startup = true,
+    header = ascii_art_1,
+  },
+}
+
 M.plugins = 'custom.plugins'
 return M
---
+++ lua/custom/dashboard.lua
@@ -0,0 +1,1 @@
+vim.g.dashboard_custom_footer = {'Powered by Neovim'}
