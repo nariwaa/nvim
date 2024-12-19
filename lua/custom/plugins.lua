529dbe1f0e0e5342a2f129e5db8915bbb4e6f420
nariwaa <86487331+nariwaa@users.noreply.github.com>
2024-12-19 09:38:42 +0100

    added lazygit

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -13,12 +13,30 @@ local plugins = {
     }
   },
   {
-      "gbprod/substitute.nvim",
+    "gbprod/substitute.nvim",
     opts = {
         -- your configuration comes here
         -- or leave it empty to use the default settings
         -- refer to the configuration section below
     }
   },
+  {
+    "kdheepak/lazygit.nvim",
+    lazy = true,
+    cmd = {
+        "LazyGit",
+        "LazyGitConfig",
+        "LazyGitCurrentFile",
+        "LazyGitFilter",
+        "LazyGitFilterCurrentFile",
+    },
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+    },
+    keys = {
+      { "<leader>gl", "<cmd>LazyGit<cr>", desc = "LazyGit" }
+    },
+  },
 }
+
 return plugins
